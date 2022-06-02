a2d038f9a6704cd939061332277219fb3bf064cd
Cameron H <chaynes2019@gmail.com>
2022-06-02 14:00:30 -0400

    I'm skeptical of my current coding site finder

--- experiments/2022-5-5-LalejiniEtAlWLogic25/AssociatedScripts/CodingSiteGeneratorHPCCCopy.py
+++ experiments/2022-5-5-LalejiniEtAlWLogic25/AssociatedScripts/CodingSiteGeneratorHPCCCopy.py
@@ -180,6 +180,7 @@ def writeTaskCodingSites(runDir,codingSites):
     with open(writeDirectory,'w') as f:
         for site in codingSites:
             f.write('{},'.format(site))
+        f.write(len(codingSites))
 
 def writeExperimentTaskCodingSites(treatmentArray):
     for treatment in treatmentArray:
