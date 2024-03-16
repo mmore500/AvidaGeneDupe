6c26aef81ac50efb480a57e461cc08b236e53f26
Cameron Haynes <chaynes2019@gmail.com>
2024-03-16 14:33:38 -0400

    Fixed path to CodingSiteGenerator in overarching script

--- experiments/2024-3-14-StarterGenomeWithNOP-X/AssociatedScripts/runInitialDataAnalysis.sh
+++ experiments/2024-3-14-StarterGenomeWithNOP-X/AssociatedScripts/runInitialDataAnalysis.sh
@@ -3,7 +3,7 @@ EXPERIMENT_ID=2024-3-14-StarterGenomeWithNOP-X
 
 EXPERIMENT_DIR=/scratch/zamanlh_root/zamanlh0/${USERNAME}/${EXPERIMENT_ID}
 
-cp ${ASSOCIATED_SCRIPTS_DIR}/CodingSiteGeneratorHPCCCopy.py ${EXPERIMENT_DIR}
+cp CodingSiteGeneratorHPCCCopy.py ${EXPERIMENT_DIR}
 
 ANALYSIS_TIME=`python3 RunGeneDuplicationAvidaAnalysisScript.py`
 
