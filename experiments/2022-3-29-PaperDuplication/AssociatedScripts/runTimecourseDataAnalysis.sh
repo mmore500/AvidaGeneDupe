9570cdce158124be607e2b661a38d7e2affddf30
Cameron Haynes <chaynes2019@gmail.com>
2024-04-03 18:06:40 -0400

    Restored SBATCH functionality -- ready to try it as an array job

--- experiments/2022-3-29-PaperDuplication/AssociatedScripts/runTimecourseDataAnalysis.sh
+++ experiments/2022-3-29-PaperDuplication/AssociatedScripts/runTimecourseDataAnalysis.sh
@@ -1,4 +1,14 @@
-SLURM_ARRAY_TASK_ID=1
+#!/bin/bash
+# The interpreter used to execute the script
+#SBATCH --job-name=broadTimecourseDuplicationDataAnalysis
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=clhaynes@umich.edu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem=400m
+#SBATCH --time=00-02:00:00
+#SBATCH --account=zamanlh0
+#SBATCH --array=1-2
 
 USERNAME=clhaynes
 EXPERIMENT_ID=2022-3-29-PaperDuplication
