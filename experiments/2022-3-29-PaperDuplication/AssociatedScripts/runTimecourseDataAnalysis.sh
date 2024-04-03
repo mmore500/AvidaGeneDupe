45069b4f73590dd2b92f8c17cad0a54923ca2e09
Cameron Haynes <chaynes2019@gmail.com>
2024-04-03 14:07:05 -0400

    Realized that, for test jobs of one, I can just remove the SBATCH interpreter bit and run it on one node. That way, I don't use lab minutes on the HPCC

--- experiments/2022-3-29-PaperDuplication/AssociatedScripts/runTimecourseDataAnalysis.sh
+++ experiments/2022-3-29-PaperDuplication/AssociatedScripts/runTimecourseDataAnalysis.sh
@@ -1,15 +1,4 @@
-#!/bin/bash
-# The interpreter used to execute the script
-
-#SBATCH --job-name=broadTimecourseDuplicationDataAnalysis
-#SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --mail-user=clhaynes@umich.edu
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --mem=400m
-#SBATCH --time=00-02:00:00
-#SBATCH --account=zamanlh0
-#SBATCH --array=1-1
+SLURM_ARRAY_TASK_ID=1000
 
 USERNAME=clhaynes
 EXPERIMENT_ID=2022-3-29-PaperDuplication
